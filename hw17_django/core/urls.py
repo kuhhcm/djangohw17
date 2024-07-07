1157439792d1328ee7fa2cf21ecf365c3add8a1b
kuhhcm <158571357+kuhhcm@users.noreply.github.com>
2024-07-07 06:34:12 +0600

    Delete hw17_django directory

--- hw17_django/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from app.views import IceCreamList, create_ice_cream
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', IceCreamList.as_view(), name='home'),
-    path('create/', create_ice_cream, name='create'),
-]
